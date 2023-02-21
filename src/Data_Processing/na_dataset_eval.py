8c9ba94bbd0341674a4ab67ab12e4cc11986cde8
MatthewlWilcox <70713614+MatthewlWilcox@users.noreply.github.com>
2023-02-20 20:25:34 -0800

    na_leauges summaryview

--- src/Data_Processing/na_dataset_eval.py
+++ src/Data_Processing/na_dataset_eval.py
@@ -11,24 +11,44 @@ print(data)
 #         print(column, data[column].isnull().sum())
 #     sys.stdout = sys.__stdout__
 
-
-
+data = data[data['year']<=2019]
+print(data)
 leagues = data['Div'].unique()
 print(leagues)
+na_columns =['div', 'size'] +list(data.columns)
+print(na_columns)
+na_dataframe = pd.DataFrame(columns = na_columns)
+print(na_dataframe)
 
 for league in leagues:
-    df = data[data['Div'] == league]
-    address = 'src/Data_processing/na_by_league/'+str(league)+'summary.txt'
-    with open(address, 'w') as f:
-        sys.stdout = f
-        print("League: ", league)
-        print('#'*30)
-        print("Years \n Max Year:", df['year'].max(), '\n Min Year', df['year'].min())
-        print('#'*30)
-        print(df.head().to_string().encode('latin-1'))
-        print('#'*30)
+  df = data[data['Div']==league]
+  na_list = [league, len(df)]
+  for column in df.columns:
+    na_list = na_list + ["%.2f" % (df[column].isnull().sum()/len(df[column])*100)]
+  print(na_list)
+  na_dataframe.loc[len(na_dataframe)] = na_list
+  
+na_dataframe.to_csv('src/Data_processing/na_leagues.csv')
+print(na_dataframe)
 
-        for column in df.columns:
-          print(column, df[column].isnull().sum())
-        sys.stdout = sys.__stdout__
-    print('DONE ', league)
+# for league in leagues:
+#     df = data[data['Div'] == league]
+#     address = 'src/Data_processing/na_by_league/'+str(league)+'summary.txt'
+#     with open(address, 'w') as f:
+#         sys.stdout = f
+#         print("League: ", league)
+#         print('#'*30)
+#         print("Years \n Max Year:", df['year'].max(), '\n Min Year', df['year'].min())
+#         print('#'*30)
+#         try:
+#           print(df.head())
+#         except:
+#           print('Error')
+#         print('#'*30)
+
+#         for column in df.columns:
+#           print(column, df[column].isnull().sum())
+#         sys.stdout = sys.__stdout__
+#     print('DONE ', league)
+
+
