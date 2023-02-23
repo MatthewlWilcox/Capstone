0c2a3ed35ca0800fad79b10258d449269aa18a86
MatthewlWilcox <70713614+MatthewlWilcox@users.noreply.github.com>
2023-02-23 00:22:23 -0500

    continue to update dataset
    continue to shrink the dataset columns and clean errors within the dataset.

--- src/Data_Processing/percent_capacity_attendance.py
+++ src/Data_Processing/percent_capacity_attendance.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import numpy as np
-data = pd.read_pickle('data/Data_to_change/complete_data_before_2019.pkl')
+import re
 
+data = pd.read_pickle('data/Data_to_change/complete_data_2010_2019_shrunk.pkl')
 
 for col in data.columns:
     print(col)
@@ -9,21 +10,39 @@ for col in data.columns:
 data = data.rename(columns = {'attendance':'raw_attendance'})
 data['Capacity'] = data['Capacity'].astype(str)
 
-data.loc[data.a_href == 'https://www.worldfootball.net/venues/mersin-arena-mersin/', 'Capacity'] = '25534'
-data.loc[data.a_href == 'https://www.worldfootball.net/venues/stade-paul-lignon-rodez/', 'Capacity'] = '5955'
+data.loc[data.url == 'https://www.worldfootball.net/venues/mersin-arena-mersin/', 'Capacity'] = '25534'
+data.loc[data.url == 'https://www.worldfootball.net/venues/stade-paul-lignon-rodez/', 'Capacity'] = '5955'
 
 
-df2 = data[~data['raw_attendance'].str.contains('\d')]
-
+df2 = data[data['away_score'].str.contains('\d')]
+print(df2)
 df2.to_csv('src/Data_processing/missing_capicty.csv')
 
+data = data[data['raw_attendance'].str.contains('\d')]
+data['raw_attendance'] = data['raw_attendance'].astype(float)
+data['Capacity'] = data['Capacity'].astype(float)
+
+def remove_non_numberics(s):
+    return re.sub('[^0-9]', '', s)
+
+data['away_score'] = data['away_score'].apply(lambda x: re.sub('[^0-9]', '', x))
 
 # print(df2['Capacity'])
 # for index, row in data.iterrows():
 #     if row['check_raw'] == 'str'
 # print(data[['Capacity', 'raw_attendance']])
-data[['raw_attendance', 'Capacity']].astype(int)
-# print(data[['raw_attendance', 'Capacity']].dtypes)
-data['capacity_filled'] = data['raw_attendance']/data['Capacity']
 
-print(data['capacity_filled'])
+
+# print(data[['raw_attendance', 'Capacity']].dtypes)
+data['capacity_filled'] = data['raw_attendance']/data['Capacity']*100
+data['capacity_filled'] = data['capacity_filled'].round(4)
+data['home_score']= data['home_score'].astype(int)
+print(data.dtypes)
+
+data['away_score']= data['away_score'].astype(int)
+
+print(data.dtypes)
+
+data.to_pickle('data/final_datasets/Total_data.pkl')
+
+data.to_csv('data/final_datasets/Total_data.csv')
--- src/Data_Processing/shrink_dataset.py
+++ src/Data_Processing/shrink_dataset.py
@@ -1,39 +1,45 @@
 import pandas as pd
 
 
-data = pd.read_pickle('data/Data_to_change/complete_data.pkl')
+data = pd.read_pickle('data/Data_to_change/complete_data_before_2019.pkl')
 
 
-data = data[['home_team','home_score','away_team','away_score','day_of_week','month','day_of_month','year','time','attendance','stadium_x','a_href','Hohome_teammeTeam','date','Div','Date','HomeTeam','AwayTeam','FTHG','FTAG','FTR','HTHG','HTAG','HTR','B365H','B365D','B365A','BWH','BWD','BWA','WHH','WHD','WHA','VCH','VCD','VCA','BbMx>2.5','BbAv>2.5','BbMx<2.5','BbAv<2.5','index','stadium_y','City','Country','Capacity','url','_merge']]
+data = data[['home_team','away_team','home_score','away_score','date','time','day_of_week','attendance','stadium_y','City','Country','Capacity','url','Div','FTHG','FTAG','FTR','HTHG','HTAG','HTR','B365H','B365D','B365A','BWH','BWD','BWA','WHH','WHD','WHA','VCH','VCD','VCA','BbMx>2.5','BbAv>2.5','BbMx<2.5','BbAv<2.5']]
 
+print(data)
+
+
+data.to_pickle("data/Data_to_change/complete_data_2010_2019_shrunk.pkl")
+data.to_csv("data/Data_to_change/complete_data_2010_2019_shrunk.csv")
+
+
+# x = 0
+
+# data2 = data[data['year']<= 2019]
 # print(data)
-x = 0
-
-data2 = data[data['year']<= 2019]
-
-div= []
-for index, row in data2.iterrows():
-    if  row['home_team'] != row['HomeTeam']:
-        division = row['Div']
-        div = div + [division]
-        x += 1
+# div= []
+# for index, row in data2.iterrows():
+#     if  row['home_team'] != row['HomeTeam']:
+#         division = row['Div']
+#         div = div + [division]
+#         x += 1
 
 
 
 
 
-print(div)
+# print(len(div))
 
 
-counts={}
-for i in div:
-    if i in counts:
-        counts[i] = counts[i] + 1
-    else:
-        counts[i] = 1
+# counts={}
+# for i in div:
+#     if i in counts:
+#         counts[i] = counts[i] + 1
+#     else:
+#         counts[i] = 1
 
 
-print(counts)
+# # print(counts)
 
 
 
