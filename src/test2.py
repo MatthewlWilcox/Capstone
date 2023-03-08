e14696514ecc93b496bffa48af0c573b9a81e2ba
MatthewlWilcox <70713614+MatthewlWilcox@users.noreply.github.com>
2023-03-08 09:37:01 -0800

    Presenation
    Tuesday work on presentaiton

--- src/test2.py
+++ src/test2.py
@@ -1,6 +1,24 @@
 import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-x = pd.read_pickle('src\Data_Processing\Key_dictionary.pkl')
-df = pd.DataFrame.from_dict(x,orient = 'index')
-print(df)
-df.to_csv('src/Data_Processing/key_dictionary_view.csv')
+
+
+time_df = pd.read_pickle('d:/Python Work/Capstone/data/final_datasets/time_shrink.pkl')
+
+df_grouped_median_tod= time_df.groupby(time_df['date_time'].dt.hour).median()
+
+
+
+df_grouped_count = time_df.groupby(time_df['date_time'].dt.hour).count()
+df_grouped_count = df_grouped_count['raw_attendance'].reset_index()
+# df_grouped_count= df_grouped_count.rename(columns = {'date':'count'})
+print(df_grouped_count)
+# sns.lineplot(data = df_grouped_median_tod, x = 'date_time', y = 'raw_attendance', markers = True, marker = "o" )
+# ax2 = plt.twinx()
+sns.lineplot(data = df_grouped_count, x = 'date_time', y = 'raw_attendance', markers = True, marker = "o" )
+sns.lineplot(data = df_grouped_median_tod, x = 'date_time', y = 'raw_attendance', markers = True, marker = "o" )
+
+
+plt.show()
