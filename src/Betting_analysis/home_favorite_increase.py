dfb2a042f2a0bc2eeae1180bacd4c1ffd616b5c5
MatthewlWilcox <70713614+MatthewlWilcox@users.noreply.github.com>
2023-03-26 22:26:47 -0400

    Home_fav and predicted

--- src/Betting_analysis/home_favorite_increase.py
+++ src/Betting_analysis/home_favorite_increase.py
@@ -7,25 +7,50 @@ bet_data = bet_data.dropna(subset = ['B365H', 'B365A'])
 h_fav = []
 attend_increase = []
 for index, rows in bet_data.iterrows():
-    if rows['B365H'] > rows['B365A']:
-        h_fav = h_fav + [1]
+    if rows['B365H'] < rows['B365A']:
+        h_fav = h_fav + [True]
     else:
-        h_fav = h_fav + [0]
+        h_fav = h_fav + [False]
     if rows['standard_attend'] >= 0:
-        attend_increase = attend_increase + [1]
+        attend_increase = attend_increase + [True]
     else:
-        attend_increase = attend_increase + [0]
+        attend_increase = attend_increase + [False]
 print(h_fav)
         
 bet_increase_bool = bet_data
 bet_increase_bool['home_fav'] = h_fav
 bet_increase_bool['attend_increase'] = attend_increase
 bet_bool = bet_increase_bool[['home_fav', 'attend_increase','raw_attendance']]
-bet_bool = bet_bool.groupby(['home_fav', 'attend_increase']).count().reset_index()
-bet_bool = bet_bool.pivot_table(values = 'raw_attendance', index = 'home_fav', columns = 'attend_increase')
+bet_bool = bet_bool.groupby(['home_fav', 'attend_increase']).count().reset_index().rename(columns = {'home_fav':'Home Team Favorited', 'attend_increase':'Increase In Attendance'})
+# total_matches = sum(bet_bool['raw_attendance'])
+# bet_bool['raw_attendance'] = bet_bool['raw_attendance'] *100/total_matches
+
+print(bet_bool)
+# print(total_matches)
+count_pct = []
+for index, rows in bet_bool.iterrows():
+    print('----------')
+    goals = rows['Home Team Favorited']
+    print(goals)
+    total_count_goals = bet_bool[bet_bool['Home Team Favorited'] == goals]['raw_attendance'].values
+    total_count_goals = sum(total_count_goals)
+    print(total_count_goals)
+    percent_total = rows['raw_attendance']*100/total_count_goals
+    print(percent_total)
+    count_pct = count_pct + [percent_total]
+bet_bool['pcnt_values']  = count_pct
+
+
+# bet_bool = bet_bool.pivot_table(values = 'pcnt_values', index = 'Home Team Favorited', columns = 'Increase In Attendance')
 print(bet_bool)
 
-sns.heatmap(bet_bool)
+# sns.heatmap(bet_bool,annot= True)
+sns.barplot(data =bet_bool, x = 'Home Team Favorited', y = 'pcnt_values', hue = 'Increase In Attendance')
+
+plt.gca().invert_xaxis()
+# plt.gca().invert_yaxis()
+plt.title('Attendance Impact depending if Home Team is Favored')
+plt.ylabel('Percent')
 plt.show()
 
 
--- src/Betting_analysis/predicted_goals.py
+++ src/Betting_analysis/predicted_goals.py
@@ -14,7 +14,7 @@ for index, rows in data.iterrows():
         increase_attend = increase_attend + [True]
     else:
         increase_attend = increase_attend + [False]
-    if rows['BbAv>2.5'] >= rows['BbAv<2.5']:
+    if rows['BbAv>2.5'] <= rows['BbAv<2.5']:
         higher_goals = higher_goals + [True]
     else:
         higher_goals = higher_goals +[False]
@@ -36,21 +36,28 @@ for index, rows in df.iterrows():
     goals = rows['higher_goals']
     print(goals)
     total_count_goals = df_grouped_higher_goal_sum[df_grouped_higher_goal_sum['higher_goals'] == goals]['count'].values
-    total_count_goals = total_count_goals[0]
+    total_count_goals = sum(total_count_goals)
     print(total_count_goals)
-    percent_total = rows['count']/total_count_goals
+    percent_total = rows['count']*100/total_count_goals
     print(percent_total)
     count_pct = count_pct + [percent_total]
 df['count_pct'] = count_pct
 # total_count = df['count'].sum()
 # df['pct'] = df['count']*100/total_count
-df = df.rename(columns = {'higher_goals':'Odds of Goals over 2.5 is greater', 'increase_attend': 'Increased Attendnace'})
-df2 = df.pivot(index = 'Odds of Goals over 2.5 is greater', columns = 'Increased Attendnace', values = 'count_pct')
+df = df.rename(columns = {'higher_goals':'Odds of Goals over 2.5 is greater', 'increase_attend': 'Increased Attendance'})
+df2 = df.pivot(index = 'Odds of Goals over 2.5 is greater', columns = 'Increased Attendance', values = 'count_pct')
 print(df)
 sns.heatmap(data = df2, annot= True)
+plt.gca().invert_xaxis()
+# plt.gca().invert_yaxis()
 plt.title('Predicted Goals in relation to Attendance')
 plt.show()
-sns.barplot(data =df, x = 'Odds of Goals over 2.5 is greater', y = 'count_pct', hue = 'Increased Attendnace')
+sns.barplot(data =df, x = 'Odds of Goals over 2.5 is greater', y = 'count_pct', hue = 'Increased Attendance')
+plt.gca().invert_xaxis()
 plt.ylabel('Percent')
 plt.title('Predicted Goals in relation to Attendance')
 plt.show()
+# print(df)
+# df_to_plot = df[['Odds of Goals over 2.5 is greater', 'Increased Attendance', 'count_pct']]
+# df_to_plot.set_index('Odds of Goals over 2.5 is greater').plot(kind = 'bar', stacked = True)
+# plt.show()
