37ce1af2b7ff6e6ede56202cbdbd017d6b8b733f
MatthewlWilcox <70713614+MatthewlWilcox@users.noreply.github.com>
2023-04-12 15:48:45 -0700

    push progress reports

--- src/Teams_impact/away_team_impact.py
+++ src/Teams_impact/away_team_impact.py
@@ -8,12 +8,13 @@ print(data)
 away_team_impact = data.groupby(['away_team', 'division'])['standard_attend'].mean().reset_index()
 
 
-
+away_team_impact = away_team_impact[away_team_impact['division'].isin(['E3', 'E1', 'E2'])]
 div_dict = {'D1':'Bundesliga', 'D2': '2. Bundesliga', 'E0':'Premier League', 'E1':'Championship', 
             'E2':'League 1', 'E3':'Leauge 2','SP1':'La Liga Primera', 'SP2':'La Liga Segunda',
               'B1':'Jupiler League', 'F1':'Ligue 1','F2':'Ligue 2','I1':'Serie A','I2':'Seire B', 
               'SC0':'Scotish Premier League', 'SC1':'Scotish Division 1', 'T1':'Fubol Ligi 1', 'P1': 'Liga 1'}
 divisions_list =['D1', 'D2', 'E0', 'E1', 'E2', 'E3', 'SP1' ,'SP2', 'B1', 'F1', 'F2', 'I1', 'I2', 'SC0', 'SC1', 'T1', 'P1']
+
 away_team_impact_top30 = pd.DataFrame(columns = ['away_team', 'division', 'standard_attend'])
 # print(away_team_impact_top30)
 for i in divisions_list:
@@ -31,9 +32,9 @@ away_team_impact_top30['away_team']  = away_team_impact_top30['away_team'].str.r
 away_team_impact_top30['away_team']  = away_team_impact_top30['away_team'].str.upper()
 print(away_team_impact_top30)
 sns.barplot(data = away_team_impact_top30, x = 'standard_attend', y = 'away_team', hue = 'division', dodge = False)
-plt.title('Away Teams Impact on Attendance: Top 3 Teams per League')
+plt.title('Away Teams Impact on Attendance: Top 3 Teams per English League')
 plt.xlabel('Average Increase on Home Teams Attendnace')
 plt.ylabel('Away Team')
-plt.legend(bbox_to_anchor=(1, 1), loc='upper left', borderaxespad=0)
-plt.savefig('results/Away_team_impact_top.png')
+# plt.legend(bbox_to_anchor=(1, 1), loc='upper left', borderaxespad=0)
+# plt.savefig('results/Away_team_impact_top.png')
 plt.show()
